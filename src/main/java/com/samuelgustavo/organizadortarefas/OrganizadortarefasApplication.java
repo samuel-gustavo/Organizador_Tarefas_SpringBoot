df772ac99413280281c7cb13b0c4b5c22c559be1
Samuel Gustavo <dev.samuelgustavo@gmail.com>
2025-08-15 15:28:40 -0300

    Primeiro commit 15/08/2025

---
+++ src/main/java/com/samuelgustavo/organizadortarefas/OrganizadortarefasApplication.java
@@ -0,0 +1,13 @@
+package com.samuelgustavo.organizadortarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizadortarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizadortarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samuelgustavo/organizadortarefas/OrganizadortarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuelgustavo.organizadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizadortarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
