d1d0120b5de897be6298c67bdfb69327e9b34a4d
Samuel Gustavo <dev.samuelgustavo@gmail.com>
2025-08-15 16:27:45 -0300

    Criando Controlador da login

---
+++ src/main/java/com/samuelgustavo/organizadortarefas/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.samuelgustavo.organizadortarefas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginIndex() {
+        return "login";
+    }
+    
+}
